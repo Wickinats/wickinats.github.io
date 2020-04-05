32a0ce40ef4e843c583a1de28a09c2c201c1e262
wickinats <wickinats@ya.ru>
2020-04-06 00:13:05 +1000

    Create a menubar. Need a test

---
+++ precache-manifest.6aec368c13d818aa43d10ed2c8d0a330.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e27ebf8ff9bff2346e",
+    "url": "/css/app.f9e8b6c8.css"
+  },
+  {
+    "revision": "5884e62ed88478c88e24",
+    "url": "/css/chunk-vendors.3af5fe38.css"
+  },
+  {
+    "revision": "ed8b59e59e194f0e3c6f6173d17f5191",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44bd8f1d608f200f1168",
+    "url": "/js/about.38887fb6.js"
+  },
+  {
+    "revision": "65e27ebf8ff9bff2346e",
+    "url": "/js/app.9385c2da.js"
+  },
+  {
+    "revision": "5884e62ed88478c88e24",
+    "url": "/js/chunk-vendors.0d638796.js"
+  },
+  {
+    "revision": "c6c96a23631792379bb7a0df868a773d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
